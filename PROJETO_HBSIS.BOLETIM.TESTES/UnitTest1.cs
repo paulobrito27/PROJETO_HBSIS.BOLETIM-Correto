aadddfe17648bb27830936c24844e2216884c355
Paulo José Maciel de Brito <paulojmbrito27@gmail.com>
2020-09-23 11:07:06 -0300

    Mais alguns testes

--- PROJETO_HBSIS.BOLETIM.TESTES/UnitTest1.cs
+++ PROJETO_HBSIS.BOLETIM.TESTES/UnitTest1.cs
@@ -2,6 +2,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PROJETO_HBSIS.BOLETIM.MODELS;
 using PROJETO_HBSIS.BOLETIM.VALITATOR.Validation;
 using System;
+using System.Text.RegularExpressions;
 
 namespace PROJETO_HBSIS.BOLETIM.TESTES
 {
@@ -105,5 +106,79 @@ namespace PROJETO_HBSIS.BOLETIM.TESTES
             Assert.IsFalse(result.IsValid);
         }
 
+        [TestMethod]
+        public void ValidaTamanhoePadraoCpfInformado_True()
+        {
+            Aluno aluno = new Aluno()
+            {
+                Nome = "teste",
+                Sobrenome = "teste",
+                DataNascimento = DateTime.Parse("2000 - 01 - 01T00:00:00"),
+                Cpf = "11111111111"
+            };
+            var result = new AlunoValidator().Valida(aluno);
+            Assert.IsTrue(result.IsValid);
+        }
+
+        [TestMethod]
+        public void ValidaCpfInformado_Tamanho_False()
+        {
+            Aluno aluno = new Aluno()
+            {
+                Nome = "teste",
+                Sobrenome = "teste",
+                DataNascimento = DateTime.Parse("2000 - 01 - 01T00:00:00"),
+                Cpf = "1111111111"
+            };
+            var result = new AlunoValidator().Valida(aluno);
+            Assert.IsFalse(result.IsValid);
+        }
+
+        [TestMethod]
+        public void ValidaCpfInformado_Formato_False()
+        {
+            Aluno aluno = new Aluno()
+            {
+                Nome = "teste",
+                Sobrenome = "teste",
+                DataNascimento = DateTime.Parse("2000 - 01 - 01T00:00:00"),
+                Cpf = "1111111A111"
+            };
+            var result = new AlunoValidator().Valida(aluno);
+            Assert.IsFalse(result.IsValid);
+        }
+
+        [TestMethod]
+        public void ValidaCpfFormatado_true()
+        {
+            Aluno aluno = new Aluno()
+            {
+                Nome = "teste",
+                Sobrenome = "teste",
+                DataNascimento = DateTime.Parse("2000 - 01 - 01T00:00:00"),
+                Cpf = "00734849974"
+            };
+            var cpfFormatado = new AlunoValidator().FormataCPF(aluno);
+
+            bool match = Regex.IsMatch(cpfFormatado, @"^\d{3}\.\d{3}\.\d{3}\-\d{2}$");
+            Assert.IsTrue(match);
+        }
+
+        [TestMethod]
+        public void ValidaCpfFormatado_false()
+        {
+            Aluno aluno = new Aluno()
+            {
+                Nome = "teste",
+                Sobrenome = "teste",
+                DataNascimento = DateTime.Parse("2000 - 01 - 01T00:00:00"),
+                Cpf = "007348499a4"
+            };
+            var cpfFormatado = new AlunoValidator().FormataCPF(aluno);
+
+            bool match = Regex.IsMatch(cpfFormatado, @"^\d{3}\.\d{3}\.\d{3}\-\d{2}$");
+            Assert.IsFalse(match);
+        }
     }
 }
+
